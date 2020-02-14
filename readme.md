fa6503885c176c25553afabd96b54123da6c8657
James E Ries <jer676@mail.missouri.edu>
2020-02-14 11:57:24 -0600

    Updates for SP2020

--- readme.md
+++ readme.md
@@ -1,19 +1,13 @@
 # Assignment 3
 
 Milestone 0: CMake and Unit Testing filled out 
-Due: Friday, September 20, 2019 11:59 PM
 
 Milestone 1: PCB file loading and First Come, First Served 
-Due: Friday, September 27, 2019 11:59 PM
 
-Milestone 2: Priority scheduling, Round Robin, and analysis of algorithms 
-Due: Friday, October 4, 2019 11:59 PM
+Milestone 2: Shortest Job First, Round Robin, and analysis of algorithms 
 
 Note: 
-1. After you run assignment3_test, the PCBs.bin will be created in the ./test directory.
-   so in the ./build directory, you can run the analysis program like "./analysis ../test/PCBs.bin RR 4"; or you can simply copy and paste the PCBs.bin to the ./build directory, and then run "./analysis PCBs.bin RR 4".
-     
-2. You can manually copy the time analysis from console and paste it to this readme file, but directly output from your program is strongly recommended.     
-    ---------------------------------------------------------------------------
-    Add your scheduling algorithm analysis below this line in a readable format. 
-    ---------------------------------------------------------------------------
+You can manually copy the time analysis from console and paste it to this readme file, but directly output from your program is strongly recommended.     
+---------------------------------------------------------------------------
+Add your scheduling algorithm analysis below this line in a readable format. 
+---------------------------------------------------------------------------
--- src/process_scheduling.c
+++ src/process_scheduling.c
@@ -8,36 +8,42 @@
 
 
 // private function
-void virtual_cpu(ProcessControlBlock_t *process_control_block) {
+void virtual_cpu(ProcessControlBlock_t *process_control_block) 
+{
     // decrement the burst time of the pcb
     --process_control_block->remaining_burst_time;
 }
 
-bool first_come_first_serve(dyn_array_t *ready_queue, ScheduleResult_t *result) {
+bool first_come_first_serve(dyn_array_t *ready_queue, ScheduleResult_t *result) 
+{
     // You need to remove this when you begin implementation.
     abort();  // replace me with implementation.
     return false;
 }
 
-bool priority(dyn_array_t *ready_queue, ScheduleResult_t *result) {
+bool shortest_job_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
+{
     // You need to remove this when you begin implementation.
-   abort();  // replace me with implementation.
-   return false;   
+    abort();  // replace me with implementation.
+    return false;   
 }
 
-bool round_robin(dyn_array_t *ready_queue, ScheduleResult_t *result, size_t quantum) {
+bool round_robin(dyn_array_t *ready_queue, ScheduleResult_t *result, size_t quantum) 
+{
     // You need to remove this when you begin implementation.
     abort();  // replace me with implementation.
     return false;
 }
 
-dyn_array_t *load_process_control_blocks(const char *input_file) {
+dyn_array_t *load_process_control_blocks(const char *input_file) 
+{
     // You need to remove this when you begin implementation.
     abort();  // replace me with implementation.
     return NULL;
 }
 
-bool shortest_remaining_time_first(dyn_array_t *ready_queue, ScheduleResult_t *result) {
+bool shortest_remaining_time_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
+{
     // You need to remove this when you begin implementation.
     abort();  // replace me with implementation.
     return false;
