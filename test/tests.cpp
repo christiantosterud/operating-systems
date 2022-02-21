71b1ab2e08292aa1d6a66811addca3cd3a289de9
christiantosterud <tosterud@ksu.edu>
2022-02-21 16:58:20 -0600

    <Fixed Cmake issues and added tests>

--- test/tests.cpp
+++ test/tests.cpp
@@ -11,11 +11,65 @@ extern "C"
 }
 
 
+
 #define NUM_PCB 30
 #define QUANTUM 5 // Used for Robin Round for process as the run time limit
 
 unsigned int score;
 unsigned int total;
+
+/*
+* LOAD PROCESS CONTROL BLOCKS TESTS
+**/
+
+TEST (load_process_control_blocks, NullInputFileName)
+{
+    const char *input_filename = NULL;
+    dyn_array_t *test = NULL;
+    dyn_array_t *res = load_process_control_blocks(input_filename);
+    ASSERT_EQ(res, test);
+}
+
+TEST (load_process_control_blocks, BadInputFilename)
+{
+    const char *input_filename = "/n";
+    dyn_array_t *test = NULL;
+    dyn_array_t *res = load_process_control_blocks(input_filename);
+    ASSERT_EQ(res, test);
+}
+
+TEST (load_process_control_blocks, NonExistingInputFilename)
+{
+    const char *input_filename = "nafile.binary";
+    dyn_array_t *test = NULL;
+    dyn_array_t *res = load_process_control_blocks(input_filename);
+    ASSERT_EQ(res, test);
+}
+
+/*
+* FIRST COME FIRST SERVE TESTS
+**/
+
+
+
+/* 
+* SHORTEST JOB FIRST TESTS
+**/
+
+
+/* 
+* PRIORITY TESTS
+**/
+
+
+/*
+* ROUND ROBIN TESTS
+**/
+
+
+/*
+* SHORTEST TIME REMAINING TESTS
+**/
 
 class GradeEnvironment : public testing::Environment 
 {
@@ -33,6 +87,7 @@ class GradeEnvironment : public testing::Environment
             std::cout << "SCORE: " << score << '/' << total << std::endl;
         }
 };
+
 
 
 int main(int argc, char **argv) 
